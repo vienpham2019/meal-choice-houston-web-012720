1c15c7dbd2f8277a00aed2d3c0791ce5765dfbb0
vien pham <vienpham2019@gmail.com>
2020-01-28 18:32:12 +0000

    Automatically backed up by Learn

--- meal_choice.rb
+++ meal_choice.rb
@@ -1,7 +1,7 @@
 # Define a method meal_choice that returns the meal_choice that was
 # passed into it and defaults to meat.
 
-def meal_choice(meal = "meat"){
+def meal_choice(meal = "meat")
  return meal
-}
+end
 meal_choice("veg")
